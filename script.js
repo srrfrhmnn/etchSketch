871da35e4d7a2d9c25c69ef19cc9b80e87d507b9
srrfrhmnn <srrfrhmn@gmail.com>
2021-12-19 01:51:32 -0500

    Implemented lifting pen, and keyboard shortcuts for colours

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const board = document.getElementsByClassName('board')[0];
-let clr = "black";
+let clr = "lol";
 
 for (let i = 0; i < 100*100; i++) {
     const pixels = document.createElement('div');
@@ -28,4 +28,25 @@ document.querySelectorAll("button").forEach(element => {
 
 document.getElementById("reload").addEventListener("click", () => {
     location.reload();
+});
+
+window.addEventListener("keydown", (e) => {
+    console.log(e);
+    if (e.key == "f"){
+        clr = "lol";
+    } else if(e.key == "q") {
+        clr = "red"
+    } else if(e.key == "w"){
+        clr = "blue"
+    } else if(e.key == "e"){
+        clr = "green"
+    } else if(e.key == "r"){
+        clr = "yellow"
+    } else if(e.key == "t"){
+        clr = "black"
+    } else if(e.key == "y"){
+        clr = "white"
+    } else if(e.key == "d"){
+        clr = "gray"
+    }
 });
