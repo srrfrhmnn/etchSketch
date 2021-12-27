66a41ebcdb94cff69357cf31ec305cdf5e7e4e30
srrfrhmnn <srrfrhmn@gmail.com>
2021-12-27 12:10:22 -0500

    Implemented toggling lifting pen

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const board = document.getElementsByClassName('board')[0];
 let clr = "lol";
+let lastColour = "";
 let x = 64;
+let penLifted = false;
+
 for (let i = 0; i < square(sessionStorage.getItem("gridSize")); i++) {
     const pixels = document.createElement('div');
     pixels.classList.add('pixel');
@@ -22,6 +25,7 @@ document.querySelectorAll("button").forEach(element => {
     console.log(element.id);
     element.addEventListener("click", () => {
         clr = `${element.id}`
+        penLifted = false;
         if (element.id == "erase") clr = "gray";
     })
 });
@@ -33,21 +37,39 @@ document.getElementById("reload").addEventListener("click", () => {
 window.addEventListener("keydown", (e) => {
     console.log(e);
     if (e.key == "f"){
-        clr = "lol";
+        if (penLifted == false) {
+            lastColour = clr;
+            clr = "lol";
+            penLifted = true;
+            console.log("pen lifted");
+        } else {
+            console.log("pen put down");
+            console.log(lastColour);
+            clr = lastColour;
+            penLifted = false;
+        }
+
     } else if(e.key == "q") {
         clr = "red"
+        penLifted = false;
     } else if(e.key == "w"){
         clr = "blue"
+        penLifted = false;
     } else if(e.key == "e"){
         clr = "green"
+        penLifted = false;
     } else if(e.key == "r"){
         clr = "yellow"
+        penLifted = false;
     } else if(e.key == "t"){
         clr = "black"
+        penLifted = false;
     } else if(e.key == "y"){
         clr = "white"
+        penLifted = false;
     } else if(e.key == "d"){
         clr = "gray"
+        penLifted = false;
     }
 });
 
@@ -66,3 +88,4 @@ function getGridSize() {
 
 board.setAttribute("style", "grid-template-columns: repeat(" + sessionStorage.getItem("gridSize") + ", auto); grid-template-rows: repeat(" + sessionStorage.getItem("gridSize") + ", auto);");
 console.log(sessionStorage.getItem("gridSize"));
+
